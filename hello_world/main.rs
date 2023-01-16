30b520ae4d120911c434c0a234d9a9096044dad0
Taz Uddin <getleton@gmail.com>
2023-01-16 10:21:55 +0600

    Add new example

--- hello_world/main.rs
+++ hello_world/main.rs
@@ -1,9 +1,16 @@
 fn main() {
     println!("hello world");
-    let mut x;
-    // assert_eq!(x, 42);
-    x = 42;
-    let y = &x;
-    x = 43;
+    // let mut x;
+    // // assert_eq!(x, 42);
+    // x = 42;
+    // let y = &x;
+    // x = 43;
     // assert_eq!(*y, 42);
+    //
+
+
+    let x = 10;
+    let y = x;
+
+    println!("x : {} y: {}", x, y);
 }
---
+++ hello_world/ownership.rs
@@ -0,0 +1,33 @@
+fn main() {
+    let x1 = 42;
+    let y1 = Box::new(84);
+
+    {
+        let z = (x1, y1);
+    }
+
+    let x2 = x1;
+    // This is going to fail
+    // let y2 = y1;
+
+
+    let input = 1;
+    let mut output = 0;
+    noalias(&input, &mut output);
+}
+
+
+fn cache(input: &i32, sum: &mut i32) {
+    *sum = *input + *input;
+    assert_eq!(*sum, 2 * *input);
+}
+
+fn noalias(input: &i32, output: &mut i32) {
+    if *input == 1 {
+        *output = 2;
+    }
+
+    if *input != 1 {
+        *output = 3;
+    }
+}
